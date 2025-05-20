8f0c487d853d27eb3be78707dbb25e4641e88c7f
EVANGLarissa <129564408+EVANGLarissa@users.noreply.github.com>
2025-05-20 18:15:56 -0300

    Update num.primos.js

--- num.primos.js
+++ num.primos.js
@@ -1,18 +1,18 @@
 function ehPrimo(n) {
-    
     if (typeof n !== 'number') {
-        throw new Error("Digite um número.");
+        throw new Error("O parâmetro deve ser um número.");
     }
-    if (n < 2) {
+    
+    if (n <= 1) {
         return false;
     }
-
-    for (let i = 2; i < Math.sqrt(n); i++) {
+    
+    for (let i = 2; i < n; i++) {
         if (n % i === 0) {
             return false;
         }
     }
-
+    
     return true;
 }
 
