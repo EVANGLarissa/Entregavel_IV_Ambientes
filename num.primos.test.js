c11a619755e1bf61188df5fad759ea1f20d39541
EVANGLarissa <129564408+EVANGLarissa@users.noreply.github.com>
2025-05-20 18:16:09 -0300

    Update num.primos.test.js

--- num.primos.test.js
+++ num.primos.test.js
@@ -1,3 +1,20 @@
-git remote add origin https://github.com/EVANGLarissa/Entregavel_IV_Ambientes.git
-git branch -M main
-git push -u origin main
+const ehPrimo = require('./num.primos');
+
+describe('Testes para o algoritmo de números primos', () => {
+    test('Deve retornar true para o número 7', () => {
+        expect(ehPrimo(7)).toBe(true);
+    });
+
+    test('Deve retornar false para o número 4', () => {
+        expect(ehPrimo(4)).toBe(false);
+    });
+
+    test('Deve retornar false para o número 1', () => {
+        expect(ehPrimo(1)).toBe(false);
+    });
+
+    test('Deve lançar erro quando o parâmetro não for um número', () => {
+        expect(() => ehPrimo('a')).toThrow('O parâmetro deve ser um número.');
+        expect(() => ehPrimo(null)).toThrow('O parâmetro deve ser um número.');
+    });
+});
